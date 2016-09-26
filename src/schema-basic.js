cbbdc1a6915047bc169d8fca7821fc07a31c0e35
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-26 22:03:31 +0200

    Don't treat <b style=font-weight: normal> as strong
    Because Google Docs spits out such HTML to the clipboard.
    
    Closes #459

--- src/schema-basic.js
+++ src/schema-basic.js
@@ -120,7 +120,11 @@ const marks = {
   },
 
   strong: {
-    parseDOM: [{tag: "b"}, {tag: "strong"},
+    parseDOM: [{tag: "strong"},
+               // This works around a Google Docs misbehavior where
+               // pasted content will be inexplicably wrapped in `<b>`
+               // tags with a font-weight normal.
+               {tag: "b", getAttrs: node => node.style.fontWeight != "normal" && null},
                {style: "font-weight", getAttrs: value => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null}],
     toDOM() { return ["strong"] }
   },
