6521792c10edbaad4d2d10a0b21baa1b56364e82
Marijn Haverbeke <marijn@haverbeke.nl>
2023-01-18 11:08:03 +0100

    Add clearing parse rules for strong and em
    FIX: Add parse rules to clear `em` and `strong` marks for styles that reset
    font style/weight.
    
    Closes https://github.com/ProseMirror/prosemirror/issues/1347

--- src/schema-basic.ts
+++ src/schema-basic.ts
@@ -125,19 +125,26 @@ export const marks = {
   /// An emphasis mark. Rendered as an `<em>` element. Has parse rules
   /// that also match `<i>` and `font-style: italic`.
   em: {
-    parseDOM: [{tag: "i"}, {tag: "em"}, {style: "font-style=italic"}],
+    parseDOM: [
+      {tag: "i"}, {tag: "em"},
+      {style: "font-style=italic"},
+      {style: "font-style=normal", clearMark: m => m.type.name == "em"}
+    ],
     toDOM() { return emDOM }
   } as MarkSpec,
 
   /// A strong mark. Rendered as `<strong>`, parse rules also match
   /// `<b>` and `font-weight: bold`.
   strong: {
-    parseDOM: [{tag: "strong"},
-               // This works around a Google Docs misbehavior where
-               // pasted content will be inexplicably wrapped in `<b>`
-               // tags with a font-weight normal.
-               {tag: "b", getAttrs: (node: HTMLElement) => node.style.fontWeight != "normal" && null},
-               {style: "font-weight", getAttrs: (value: string) => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null}],
+    parseDOM: [
+      {tag: "strong"},
+      // This works around a Google Docs misbehavior where
+      // pasted content will be inexplicably wrapped in `<b>`
+      // tags with a font-weight normal.
+      {tag: "b", getAttrs: (node: HTMLElement) => node.style.fontWeight != "normal" && null},
+      {style: "font-weight=400", clearMark: m => m.type.name == "strong"},
+      {style: "font-weight", getAttrs: (value: string) => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null},
+    ],
     toDOM() { return strongDOM }
   } as MarkSpec,
 
